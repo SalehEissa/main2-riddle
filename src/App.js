f5196c57a83664b0c34db9b9355fc228430b9846
Saleh Eissa <saleheissa@Salehs-MBP.attlocal.net>
2023-08-24 16:33:02 -0500

    changes app.js

--- src/App.js
+++ src/App.js
@@ -3,3 +3,17 @@ import React from "react";
 import Quote from "./components/Quote";
 import "./styles.css"; // Import the CSS file
 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Random Riddle Generator</h1>
+      </header>
+      <main>
+        <Quote />
+      </main>
+    </div>
+  );
+}
+
+export default App;
